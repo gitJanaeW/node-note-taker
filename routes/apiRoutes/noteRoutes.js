d03e215aff9890c5a501c071727fc32d68ccde01
janae.wel <janae.wel@gmail.com>
2022-08-08 22:44:34 -0400

    front end route connected

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,17 +1,22 @@
+const path = require('path');
 const router = require('express').Router();
+const {v4: uuidv4} = require('uuid');
 const {notes} = require('../../db/notes.json');
 const { validateNote, createNote } = require('../../lib/notes');
 
-// take information from front to create a new note and push to JSON
-router.post('/notes', (req, res) => {
-    console.log(req.body);
-    req.body.id = notes.length.toString();
-    if (!validateNote(req.body)) {
-        res.status(400).send('Note not properly formatted.');
-    } else {
-        const note = createNote(req.body, notes);
-        res.json(req.body);
-    }
+// api to get db/notes.json and send info to front end
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "../../db/notes.json"));
 });
 
-module.exports = router;
+// take information from frontend to create a new note and push to notes.json
+router.post('/notes', ({body}, res) => {
+    if (!validateNote) {
+        res.status(400).send('Note not properly formatted');
+        return;
+    }
+    // body.id = uuidv4;
+    const note = createNote(body, JSON.parse(notes));
+});
+
+// module.exports = router; PUT THIS BACK IF IT DOESN'T WORK
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 
+// express middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`API server now on ${PORT}`));;;
+// direct server to the routes/index.js file (which acts as a hub for all server endpoints)
+app.use(routes);
+
+
+app.listen(PORT, () => console.log(`API server now on ${PORT}`));
