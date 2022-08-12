114ba8e1951819171e9ab5c7f9436edb15a05b98
janae.wel <janae.wel@gmail.com>
2022-08-12 17:04:08 -0400

    finished product

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Node Note Taker
+    
+## Description
+
+A note taker app allows you to add and delete notes with ease!
+
+## Technologoies
+- JavaScript
+- Node.js
+- Express.js
+
+## Installation
+
+To install, clone the file. Make sure you have Node.js and unik-id installed to proceed.
+
+## Contributing
+
+Please feel free to suggest ways to make this project MORE than amazing!!
+
+## Tests
+
+To test the product, run the file in your terminal from the root folder with the command "node .\server.js" and then open the app @ the URL "localhost:3000".
+
+## Questions
+
+Please reach out with questions via GitHub or email:
+- github.com/gitJanaeW
+- janae.wel@gmail.com
+
+![](./public/assets/images/READMEdemo.gif)
--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { finished } = require('stream');
 const router = require('express').Router();
 let notes = require('../../db/notes.json');
 const {validateNote, createNote} = require('../../lib/notes');
@@ -18,11 +19,14 @@ router.post('/notes', ({body}, res) => {
     const note = createNote(body, notes);
     fs.writeFileSync(path.join(__dirname, '../../db/notes.json'),
         JSON.stringify(note), null, 2);
+    console.log("finished!");
+    res.json({
+        message: 'success',
+        data: body
+    });
 });
 
 router.delete('/notes/:id', (req, res) => {
-    console.log("req.params.id", req.params.id);
-    console.log("notes", notes);
     if (!req.params.id) {
         res.status(500).json({error: res.message});
         return;
@@ -31,7 +35,6 @@ router.delete('/notes/:id', (req, res) => {
         return note.id !== req.params.id;
     });
     notes = newNotes;
-    console.log("newNotes", newNotes);
     fs.writeFileSync(path.join(__dirname, '../../db/notes.json'),
         JSON.stringify(newNotes), null, 2);
     console.log("post sync notes", notes);
